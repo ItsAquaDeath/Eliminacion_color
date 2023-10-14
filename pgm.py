c243412f7aab1d6d1da95be11972e4e7bee545cd
lauturgi <147841669+lauturgi@users.noreply.github.com>
2023-10-14 18:18:05 +0200

    Update pgm.py

--- pgm.py
+++ pgm.py
@@ -37,9 +37,9 @@ def main():
     nombre_archivo = nombre_archivo.split('.')[:-2]
     nombre_archivo = '.'.join(nombre_archivo)
 
-    lista_prueba = [1]*36
-    alto = 6
-    ancho = 6
+    lista_prueba = [1]*2133
+    alto = 2133
+    ancho = 2133
     imagen = pgm(ancho, alto, lista_prueba)
 
     out = open(nombre_archivo+'.lenagris.pgm', 'w')
